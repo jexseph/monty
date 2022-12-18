fd8b13b011bd31ae469edc06bd4c68a74be7a30a
Ifeoma Adigwe <ifeomaadigwe91@gmail.com>
2022-12-18 12:08:02 -0800

    p_str.c

--- p_str.c
+++ p_str.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 
 /**
-* f_pstr - function that prints the string starting at the top of the stack,
-* followed by a new
+* f_pstr - functions that prints the string starting at the top of the stack,
+* followed by a new line
 * @head: head of stack
 * @counter: line count
 *
